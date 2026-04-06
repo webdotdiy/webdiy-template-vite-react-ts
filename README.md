c4d18223079022c2318549075e0cf122a37dc10e
Adesh Chopade <adeshchopade@gmail.com>
2026-04-06 10:49:23 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# React + Vite + TypeScript + Tailwind
+
+## Setup
+
+```sh
+npm install
+```
+
+## Commands
+
+```sh
+npm run dev      # development server
+npm run build    # production build
+npm run preview  # preview production build
+npm run lint     # ESLint
+```
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <p className="text-xl text-gray-600">
+        This is a blank React + Vite + TypeScript template, ask WebDIY to build your app
+      </p>
+    </div>
+  )
+}
+
+export default App
